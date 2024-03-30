8000581c993cf58b6b60473d88a4043bc86683b6
tsunami <@frodohas>
2024-03-30 19:52:02 +0300

    add mvmnt for Queen

--- src/main/java/ru/kislyakow/Main.java
+++ src/main/java/ru/kislyakow/Main.java
@@ -2,7 +2,7 @@ package ru.kislyakow;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = BoardFactory.fromFen("8/p5b1/8/8/3B3p/2q5/5n2/8");
+        Board board = BoardFactory.fromFen("8/p5b1/8/8/3Q3p/2q5/5n2/8");
         Game game = new Game(board);
         game.gameLoop();
 
--- src/main/java/ru/kislyakow/Pieces/Queen.java
+++ src/main/java/ru/kislyakow/Pieces/Queen.java
@@ -1,9 +1,8 @@
 package ru.kislyakow.Pieces;
 
-import ru.kislyakow.Color;
-import ru.kislyakow.Coordinates;
-import ru.kislyakow.CoordinatesShift;
+import ru.kislyakow.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Queen extends Piece{
@@ -13,6 +12,38 @@ public class Queen extends Piece{
 
     @Override
     protected Set<CoordinatesShift> getPieceMoves() {
-        return null;
+        Set<CoordinatesShift> coordinatesShifts = new HashSet<>();
+
+        for (int i = -7; i < 8; i++) {
+            coordinatesShifts.add(new CoordinatesShift(i, i));
+            coordinatesShifts.add(new CoordinatesShift(-i, i));
+
+            if (i == 0) continue;
+            coordinatesShifts.add(new CoordinatesShift(0, i));
+            coordinatesShifts.add(new CoordinatesShift(i, 0));
+        }
+
+        return coordinatesShifts;
+    }
+
+    @Override
+    protected boolean isSquareAvailableToMove(Coordinates coordinates, Board board) {
+        if (!super.isSquareAvailableToMove(coordinates, board)) return false;
+
+        boolean result = true;
+
+        for (Coordinates coordinates1 : BoardUtils.getDiagonalCoordinatesBetween(this.coordinates, coordinates)) {
+            if (!board.isSquareEmpty(coordinates1)) return false;
+        }
+
+        for (Coordinates coordinates1 : BoardUtils.getVerticalCoordinatesBetween(this.coordinates, coordinates)) {
+            if (!board.isSquareEmpty(coordinates1)) return false;
+        }
+
+        for (Coordinates coordinates1 : BoardUtils.getHorizontalCoordinatesBetween(this.coordinates, coordinates)) {
+            if (!board.isSquareEmpty(coordinates1)) return false;
+        }
+
+        return result;
     }
 }
